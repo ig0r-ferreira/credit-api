e7b3fe29102932abf9f13e5f38f73b2060fe7d9e
Igor Ferreira <iferreira.n01@gmail.com>
2024-02-10 21:04:59 -0300

    add get credit by code endpoint

--- src/main/kotlin/me/dio/credit/api/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/credit/api/controller/CreditResource.kt
@@ -5,11 +5,13 @@ import me.dio.credit.api.dto.CreditView
 import me.dio.credit.api.entity.Credit
 import me.dio.credit.api.service.impl.CreditService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import java.util.stream.Collectors
 
 @RestController
@@ -30,5 +32,13 @@ class CreditResource(
             .stream()
             .map { credit: Credit -> CreditView(credit) }
             .collect(Collectors.toList())
+    }
+
+    @GetMapping("/{creditCode}")
+    fun getByCreditCode(
+        @RequestParam(value = "customerId") customerId: Long,
+        @PathVariable creditCode: UUID): CreditView
+    {
+        return CreditView(this.creditService.findByCreditCode(customerId, creditCode))
     }
 }
--- src/main/kotlin/me/dio/credit/api/dto/CreditView.kt
+++ src/main/kotlin/me/dio/credit/api/dto/CreditView.kt
@@ -1,17 +1,24 @@
 package me.dio.credit.api.dto
 
 import me.dio.credit.api.entity.Credit
+import me.dio.credit.api.enums.Status
 import java.math.BigDecimal
 import java.util.UUID
 
 data class CreditView (
     val creditCode: UUID,
     val creditValue: BigDecimal,
-    val numberOfInstallments: Int
+    val numberOfInstallments: Int,
+    val status: Status,
+    val emailCustomer: String?,
+    val incomeCustomer: BigDecimal?
 ) {
     constructor(credit: Credit): this (
         creditCode = credit.creditCode,
         creditValue = credit.creditValue,
-        numberOfInstallments = credit.numberOfInstallments
+        numberOfInstallments = credit.numberOfInstallments,
+        status = credit.status,
+        emailCustomer = credit.customer?.email,
+        incomeCustomer = credit.customer?.income
     )
 }
