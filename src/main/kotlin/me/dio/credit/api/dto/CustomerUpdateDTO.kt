554be5b94dd8532b589aba91cc367cc37c0da9cf
Igor Ferreira <iferreira.n01@gmail.com>
2024-02-12 08:23:01 -0300

    fix CustomerUpdateDTO for partial update

--- src/main/kotlin/me/dio/credit/api/dto/CustomerUpdateDTO.kt
+++ src/main/kotlin/me/dio/credit/api/dto/CustomerUpdateDTO.kt
@@ -3,19 +3,29 @@ package me.dio.credit.api.dto
 import me.dio.credit.api.entity.Customer
 import java.math.BigDecimal
 
-data class CustomerUpdateDTO (
-    val firstName: String,
-    val lastName: String,
-    val income: BigDecimal,
-    val zipCode: String,
-    val street: String
+data class CustomerUpdateDTO(
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val income: BigDecimal? = null,
+    val zipCode: String? = null,
+    val street: String? = null
 ) {
     fun toEntity(customer: Customer): Customer {
-        customer.firstName = this.firstName
-        customer.lastName = this.lastName
-        customer.income = this.income
-        customer.address.zipcode = this.zipCode
-        customer.address.street = this.street
+        this.firstName?.let {
+            customer.firstName = it
+        }
+        this.lastName?.let {
+            customer.lastName = it
+        }
+        this.income?.let {
+            customer.income = it
+        }
+        this.zipCode?.let {
+            customer.address.zipcode = it
+        }
+        this.street?.let {
+            customer.address.street = it
+        }
         return customer
     }
 }
